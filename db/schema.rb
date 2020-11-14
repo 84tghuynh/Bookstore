660b063ff3da10e0261a53f8b825bd5495654606
84tghuynh <giangbrvt@gmail.com>
2020-11-14 16:22:33 -0600

    Migrate the Book model to database

---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_11_14_221446) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "title"
+    t.text "description"
+    t.string "publisher"
+    t.string "publish_date"
+    t.integer "number_of_pages"
+    t.text "book_url"
+    t.text "cover_s"
+    t.text "cover_m"
+    t.text "cover_l"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  add_foreign_key "books", "categories"
+end
