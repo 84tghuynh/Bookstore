fb44f760fe7a72253c52236c675677bc991f1a04
84tghuynh <giangbrvt@gmail.com>
2020-11-14 16:31:59 -0600

    Migrate Category model to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_221446) do
+ActiveRecord::Schema.define(version: 2020_11_14_222619) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2020_11_14_221446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "books", "categories"
