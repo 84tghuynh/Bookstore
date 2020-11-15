5e51c7f16282e1d2e7433ada0a889641fb9be0cb
84tghuynh <giangbrvt@gmail.com>
2020-11-15 15:48:35 -0600

    Register Order model with Active_admin

---
+++ app/admin/orders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Order do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :pst, :gst, :hst, :order_date, :customer_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:pst, :gst, :hst, :order_date, :customer_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
