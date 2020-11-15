9ab4bfc5490bd6fdd4be5ea3c8e47c49a5224a50
84tghuynh <giangbrvt@gmail.com>
2020-11-15 15:39:00 -0600

    Register Author model with Active_admin

---
+++ app/admin/authors.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Author do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :author_key, :name, :personal_name, :bio, :cover_s, :cover_m, :cover_l
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:author_key, :name, :personal_name, :bio, :cover_s, :cover_m, :cover_l]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
