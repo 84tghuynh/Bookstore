58785f2897fbc8ef80f157da4bc675572f669303
84tghuynh <giangbrvt@gmail.com>
2020-11-15 15:35:11 -0600

    Register Book model with Active_admin

---
+++ app/admin/books.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Book do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :isbn, :title, :description, :publisher, :publish_date, :number_of_pages, :book_url, :cover_s, :cover_m, :cover_l, :category_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:isbn, :title, :description, :publisher, :publish_date, :number_of_pages, :book_url, :cover_s, :cover_m, :cover_l, :category_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
