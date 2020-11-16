09e9bdcf6175275495919c8f4b6375007bac26d7
84tghuynh <giangbrvt@gmail.com>
2020-11-15 19:54:43 -0600

    Migrate the Book model with  price column to Database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_205835) do
+ActiveRecord::Schema.define(version: 2020_11_16_014623) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_205835) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "price"
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
