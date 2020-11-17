983d56d4a6ed009c791dfc6866c56da829f90590
84tghuynh <giangbrvt@gmail.com>
2020-11-16 22:07:44 -0600

    Install active_storage

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Contact do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :content
+  permit_params :title, :content
   #
   # or
   #
@@ -14,5 +13,4 @@ ActiveAdmin.register Contact do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
