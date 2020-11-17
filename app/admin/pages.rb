20d7e46e90687ee36f70a1713a5ec9324bc5cf08
84tghuynh <giangbrvt@gmail.com>
2020-11-17 13:47:00 -0600

    Adding  image attachment to pages

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,18 +1,17 @@
 ActiveAdmin.register Page do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :content, :page_type
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :content, :page_type]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :title, :content, :page_type, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size: "500") : ""
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,4 @@
 class Page < ApplicationRecord
+  validates :title, :content, :page_type, presence: true
+  has_one_attached :image
 end
