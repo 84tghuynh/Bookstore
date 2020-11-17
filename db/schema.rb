700a4190bc5bd04361c11c7d584e463c4b90bea3
84tghuynh <giangbrvt@gmail.com>
2020-11-16 21:41:20 -0600

    Migrate About model to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_031605) do
+ActiveRecord::Schema.define(version: 2020_11_17_032158) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
