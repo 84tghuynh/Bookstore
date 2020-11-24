9dd39e80b3a48b29f862b8035a0cccada0ba5c2f
84tghuynh <giangbrvt@gmail.com>
2020-11-24 11:24:58 -0600

    Generate the controller and edit views: index, show and routes, and actions of BOOKS controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  root to: "pages#index"
+  # get 'books/index'
+  # get 'books/show'
+  # using
+  resources :books, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
+  # OR
+  # get "books",     to: "books#index", as: "books" # books_path
+  # get "books/:id", to: "books#show",  as: "book"
+
   get "pages/about", to: "pages#about", as: "about"
   get "pages/contact", to: "pages#contact", as: "contact"
-  get "abouts/index"
+
+  root to: "books#index"
+
+  # Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
