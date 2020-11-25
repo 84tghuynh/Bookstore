5b03379dd1a830ff3de22a32d1474bb2639b387b
84tghuynh <giangbrvt@gmail.com>
2020-11-25 10:15:39 -0600

    Adding and develop search view for books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    category_search = params[:category].to_s
+
+    @pagy1, @books = pagy(if category_search == "0"
+                            Book.where(["title LIKE ? OR books.description LIKE ?", wildcard_search, wildcard_search])
+                          else
+                            Book.joins(:category)
+                                .where(["title LIKE ? OR books.description LIKE ?", wildcard_search, wildcard_search])
+                                .where("category_id = ?", category_search)
+                          end, items: 20)
+
+    @categories = Category.all
+  end
 end
