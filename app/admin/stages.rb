e58dadc73dc599731eb158d6be14c4c8b6fc8ddd
84tghuynh <giangbrvt@gmail.com>
2020-12-05 15:54:04 -0600

    Permit_params stage_id & name permitted write to Orders table and Stages table

--- app/admin/stages.rb
+++ app/admin/stages.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Stage do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name
+  permit_params :name
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Stage do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
